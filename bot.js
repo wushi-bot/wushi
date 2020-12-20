db2002220a9ece18ad14e6a03fd76631c325bca5
xMinota <jpichardo369@gmail.com>
2020-12-20 14:28:25 -0500

    and this

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 import * as Sentry from '@sentry/node'
 import { Integrations } from '@sentry/tracing'
-<<<<<<< HEAD
-=======
 import chalk from 'chalk'
->>>>>>> 742ccee4fa367296299d284ba31b3c6e6d190e23
 
 import Bot from './models/Bot'
 import 'dotenv/config'
@@ -19,8 +16,6 @@ import 'dotenv/config'
                             \|_________|
 */
 
-<<<<<<< HEAD
-=======
 if (!process.env.SENTRY) console.log(`${chalk.gray('>')} Cannot find SENTRY variable, error logging will not work.`)
 if (!process.env.CLIENT_SECRET) console.log(`${chalk.gray('>')} Cannot find CLIENT_SECRET variable, authentication will not work in the website.`)
 if (!process.env.DBL_TOKEN) console.log(`${chalk.gray('>')} Cannot find DBL_TOKEN variable, server posting will not work.`)
@@ -28,7 +23,6 @@ if (!process.env.TOKEN) console.log(`${chalk.gray('>')} Cannot find TOKEN variab
 if (!process.env.DOMAIN) console.log(`${chalk.gray('>')} Cannot find DOMAIN variable, authentication will not work.`)
 if (!process.env.KSOFT_TOKEN) console.log(`${chalk.gray('>')} Cannot find KSOFT_TOKEN variable, Ksoft.si commands will not work.`)
 
->>>>>>> 742ccee4fa367296299d284ba31b3c6e6d190e23
 Sentry.init({
   dsn: process.env.SENTRY,
   integrations: [
