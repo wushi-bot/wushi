5ad19247c2ffd1c773f057f9547e726767eb181c
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 09:39:00 -0500

    eugh

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,8 @@
-import chalk from 'chalk'
 import { updateStats } from '../utils/utils'
-import Web from '../web'
 
 module.exports.run = (bot) => {
   bot.logger.log('info', `[Event] Ready! Logged in as ${bot.user.username}#${bot.user.discriminator}`)
   bot.user.setActivity('.help | wushibot.xyz', { type: 'PLAYING' })
-  Web(bot)
   setInterval(async () => {
     if (bot.user.id === '755526238466080830') {
       await updateStats(bot.guilds.cache.size)
