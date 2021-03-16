4b22aa994f94a0e614a00f3dd670a69acaf3d056
Minota <34592028+xMinota@users.noreply.github.com>
2021-03-16 11:12:57 -0400

    [update] Start of economy

---
+++ src/commands/Economy/start.js
@@ -0,0 +1,27 @@
+import Command from '../../structs/command'
+import { MessageEmbed } from 'discord.js'
+ 
+class PingCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ping',
+      description: 'Gets the bot\'s latency.',
+      category: 'Meta',
+      aliases: ['ping-pong', 'ms'],
+      usage: 'ping',
+      cooldown: 3.5
+    })
+  }
+
+  async run (bot, msg, args) {
+    const message = await msg.reply('🏓 Pinging...')
+    const embed = new MessageEmbed()
+      .setColor(msg.member.roles.highest.color)
+      .setTitle(':ping_pong: Pong!')
+      .addField('🕐 Roundtrip took', `${message.createdTimestamp - msg.createdTimestamp}ms`)
+      .addField(`❤️ Heartbeat`, `${Math.round(this.client.ws.ping)}ms`)
+    message.edit(embed)
+  }
+}
+
+module.exports = PingCommand
