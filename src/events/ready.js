21a437fe11dfe7de2aca693765981fb6a30f7991
xMinota <jpichardo369@gmail.com>
2021-05-01 12:55:56 -0400

    [test] attempting to work with webhooks

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,20 @@
 import chalk from 'chalk'
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/dblHook', (req, res) => {
+  console.log(req.body)
+  res.status(200).end()
+})
+
+app.listen(process.env.PORT, () => {})
 
 module.exports.run = (bot) => {
   bot.logger.log('info', `Ready! Logged in as ${bot.user.username}#${bot.user.discriminator}`)
   console.log(chalk.black('────────────────────────────────────────────────────────────'))
   bot.user.setActivity('.help | .support', { type: 'LISTENING' })
+
 }
