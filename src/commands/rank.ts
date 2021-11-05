6ba89f4de71bc8d863d16a68adf7b87b6a9c5ee1
xMinota <jpichardo369@gmail.com>
2021-11-04 23:35:12 -0400

    :bug: Fix/finish rank cards & fix database utils.

--- src/commands/rank.ts
+++ src/commands/rank.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction } from 'discord.js'
-import { Canvas } from 'canvas-constructor'
+import { Canvas, registerFont, resolveImage } from 'canvas-constructor/cairo'
 import req from '@aero/centra'
 import { emojis } from '../utils/constants'
 
@@ -25,9 +25,8 @@ class RankCommand extends Command {
     const user = interaction.options.getUser('user') || interaction.member.user
     if (user.bot) {
       return await interaction.reply({ content: `${emojis.error} You cannot see the rank of bots, because they cannot gain EXP.`, ephemeral: true })
-    }
+    } // @ts-ignore
     const avatarURL = user.avatarURL({ format: 'png' })
-    const avatar = await req(avatarURL).raw()
     let member = await getMember(user.id, interaction.guild.id)
     if (!member) {
       return await interaction.reply({ content: `${emojis.error} This user has not gained EXP.`, ephemeral: true })
@@ -39,34 +38,39 @@ class RankCommand extends Command {
     let userProfile = await getUser(user.id)
     let rankCardColor = userProfile.rankCardColor || '#ff3f38'
     const canvas = new Canvas(600, 300)
-    const bg = await req('https://cdn.discordapp.com/attachments/777628711256064030/828002823185629264/a9620396df0e63802087054fbffabe931ab4de15.png').raw()
+    registerFont('./src/resources/fonts/Inter-ExtraBold.ttf', {
+      family: 'Default Bold'
+    })
+    registerFont('./src/resources/fonts/Inter-Medium.ttf', {
+      family: 'Default'
+    })
+    const bg = await resolveImage('https://cdn.discordapp.com/attachments/777628711256064030/828002823185629264/a9620396df0e63802087054fbffabe931ab4de15.png')
+    const avatar = await resolveImage(avatarURL)
     canvas
-      .addImage(bg, 0, 0, 600, 300)
+      .printImage(bg, 0, 0, 600, 300)
       .setShadowColor('#44474d')
       .setShadowBlur(10)
       .setShadowOffsetY(5)
-      .addCircle(80, 90, 48)
+      .printCircle(80, 90, 48)
       .restore()
-      .addCircularImage(avatar, 80, 90, 48, true)
-      .addTextFont('./src/resources/fonts/Inter-ExtraBold.ttf', 'Default Bold')
-      .addTextFont('./src/resources/fonts/Inter-Medium.ttf', 'Default')
+      .printCircularImage(avatar, 80, 90, 48)
       .setColor('#ffff')
       .setTextFont('36pt Default')
-      .addText(user.username, 140, 94)
+      .printText(user.username, 140, 94)
       .setTextFont('18pt Default Bold')
       .setColor('#cfd1d0')
-      .addText(`#${user.discriminator}`, 140, 122)
+      .printText(`#${user.discriminator}`, 140, 128)
       .setColor('#ffff')
       .setTextFont('24pt Default Bold')
       .setStroke('#fff')
       .setTextFont('25pt Default Bold')
-      .addText(`Level ${level}`, 85, 216)
-      .addText(`${points} / ${nextLevel}`, 390, 216)
-      .addBeveledRect(85, 230, 450, 24, 32)
+      .printText(`Level ${level}`, 85, 216)
+      .printText(`${points} / ${nextLevel}`, 390, 216)
+      .resetShadows()
+      .printRoundedRectangle(85, 230, 450, 24, 32)
       .restore()
       .setColor(rankCardColor)
-      .addBeveledRect(91, 234, progBar, 16, 20)
-      .resetShadows()
+      .printRoundedRectangle(91, 234, progBar, 16, 20)
       .clip()
     await interaction.reply({ files: [{ attachment: canvas.toBuffer(), name: 'card.png' }] })
   }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,23 +4,46 @@ import UserSchema from '../schemas/User'
 import { Guild } from "discord.js";
 
 export async function getGuild(guild: Guild) {
-  const result = await GuildSchema.findOne({
+  let result = await GuildSchema.findOne({
     id: guild.id
   }).exec()
+  if (!result) {
+    result = new GuildSchema({
+      id: guild.id
+    })
+    result.save()
+  }
   return result
 }
 
 export async function getMember(member, guildId) {
-  const result = await MemberSchema.findOne({
+  let result = await MemberSchema.findOne({
     userId: member,
     guildId: guildId
   }).exec()
+  if (!result) {
+    result = new MemberSchema({
+      userId: member,
+      guildId: guildId, 
+      expNeeded: 100,
+      exp: 0,
+      level: 0,
+      totalExp: 0
+    })
+    result.save()
+  }
   return result
 }
 
 export async function getUser(user) {
-  const result = await UserSchema.findOne({
+  let result = await UserSchema.findOne({
     id: user
   }).exec()
+  if (!result) {
+    result = new UserSchema({
+      id: user
+    })
+    result.save()
+  }
   return result
 }
