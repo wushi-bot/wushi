4c7bbc312e76472ec28dae6a98b8725fd1e6faa0
xMinota <jpichardo369@gmail.com>
2021-11-05 00:10:43 -0400

    :sparkles: Add util functions, owner-only stats & leveling up via messaging.

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -13,7 +13,7 @@ class StatsCommand extends Command {
   }
 
   async execute(interaction: CommandInteraction, client: Bot) {
-    await interaction.reply({ content: `${interaction.client.user.username} is currently using **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB** of memory & is using **wushi ${client.version}**, **Node.js ${process.version}**, **Discord.js ${version}**`, ephemeral: true })
+    await interaction.reply({ content: `:sushi: **${interaction.client.user.username}** is currently using **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB** of memory & is using **wushi ${client.version}**, **Node.js ${process.version}**, **Discord.js ${version}**. ` + `${client.owners.includes(interaction.member.user.id) ? `The bot currently is caching **${client.users.cache.size.toLocaleString()}** users, **${client.guilds.cache.size.toLocaleString()}** guilds, **${client.channels.cache.size.toLocaleString()}** channels` : ' ' }`, ephemeral: true })
   }
 }
 
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,6 +1,47 @@
-import { Message } from "discord.js";
+import { Message, Collection } from "discord.js";
+import Guild from '../schemas/Guild'
+import { getMember } from '../utils/database'
+import { getRandomInt } from '../utils/functions'
 import Bot from "../models/Client";
+
+const expCooldowns = new Collection()
 
 exports.run = async (bot: Bot, message: Message) => {
   if (message.author.bot) return
+  if (message.webhookId) return
+  let guild = await Guild.findOne({
+    id: message.guild.id
+  }).exec()
+  if (guild && guild.leveling) {
+    if (!expCooldowns.has(message.author.id)) {
+      let member = await getMember(message.author.id, message.guild.id)
+      let exp = await getRandomInt(6, 16)
+      member.exp += exp
+      member.totalExp += exp
+      bot.logger.info(`Added ${Math.floor(exp)} exp to ${message.author.username}#${message.author.discriminator} (${message.author.id}) in ${message.guild.id}.`)
+      if (member.expNeeded <= member.exp) {
+        member.level += 1
+        member.exp -= member.expNeeded 
+        member.expNeeded = Math.floor(member.expNeeded + (member.expNeeded * 0.1))
+        if (!guild.levelUpMessage) { 
+          try {
+            message.channel.send(`Congratulations, **${message.author.username}**, you've leveled :up: to **Level ${member.level}**!`)
+          } catch (e) {}
+          
+        } else {
+          let lvlMsg = guild.levelUpMessage
+          lvlMsg = lvlMsg.replace('{level}', member.level)
+          lvlMsg = lvlMsg.replace('{user.name}', `${message.author.username}`)
+          lvlMsg = lvlMsg.replace('{user.mention}', `<@!${message.author.id}>`)
+          lvlMsg = lvlMsg.replace('{user.id}', `${message.author.id}`)
+          lvlMsg = lvlMsg.replace('{user.discrim}', `${message.author.discriminator}`)
+          lvlMsg = lvlMsg.replace('{nextExp}', member.expNeeded)
+          try {
+            message.channel.send(lvlMsg)
+          } catch (e) {}
+        }
+      }
+      member.save()
+    }
+  }
 }
