a35132f680fd9d17118ed78f008257cc3f75e66c
Juan <jpichardo369@gmail.com>
2021-11-17 00:49:33 -0500

    :sparkles: Add the ability to bulk-buy items

--- src/commands/buy.ts
+++ src/commands/buy.ts
@@ -28,25 +28,29 @@ class BuyCommand extends Command {
 
   async execute(interaction: CommandInteraction, client: Bot) {
     let items = await getItems()
+    let quantity = interaction.options.getInteger('amount') || 1
     let item = interaction.options.getString('item').toLowerCase()
     if (!items.some(i => i.id === item)) {
       return await interaction.reply({ content: 'Invalid item, try again!', ephemeral: true })
     }
     let user = await getUser(interaction.member.user.id)
     item = await getItem(item) // @ts-ignore
-    if (item.price > user.balance) { // @ts-ignore
+    if (item.price * quantity > user.balance) { // @ts-ignore
       return await interaction.reply({ content: `You don't have enough for this item. You are short :coin: **${await addCommas(item.price - user.balance)}**.`})
     } // @ts-ignore
     if (item.max) { // @ts-ignore
       if (user.items[item.id] >= item.max) {
         return await interaction.reply({ content: `You already have too much of that item. Try buying another item!`, ephemeral: true })
+      } // @ts-ignore
+      if (user.items[item.id] + quantity > item.max) {
+        return await interaction.reply({ content: `Buying this amount of items would set you over the maximum amount of items, please try a lower number.`, ephemeral: true })
       }
     }
     // @ts-ignore
-    user.balance -= item.price 
+    user.balance -= item.price * quantity
     if (!user.items) user.items = {} // @ts-ignore
     let amount = user.items[item.id] || 0
-    amount += 1 // @ts-ignore
+    amount += quantity // @ts-ignore
     user.items[item.id] = amount
     user.markModified('items') // @ts-ignore
     if (item.type === 'rod') { // @ts-ignore
@@ -55,7 +59,7 @@ class BuyCommand extends Command {
     }
     user.save()
     await interaction.reply({ // @ts-ignore
-      content: `Successfully purchased ${item.emoji} **${item.display}**! | Description: ${item.description}` 
+      content: `<:check:820704989282172960> Successfully purchased **${quantity}** ${item.emoji} **[${item.display}](https://dummylinkforthetimebeing.com)**, your new balance is now :coin: **${await addCommas(Math.floor(user.balance))}**! | ${item.description}` 
     })
     
   }
